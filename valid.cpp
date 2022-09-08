ea69446a8d6ed0c5e2ed5514063e31a8b3ea5ed8
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-08 16:09:42 -0400

    Update valid.cpp

--- valid.cpp
+++ valid.cpp
@@ -1,12 +1,36 @@
 /*
 Author: Frida Mesa
 Course: CSCI-135
-Instructor: Micheal Zamansky
-Assignment: Lab2 A
+Instructor: Michael Zamansky
+Assignment: Lab 2 A
 
-Here, briefly, at least in one or a few sentences
-describe what the program does.
+The program asks the user for an integer between 1 and 100 and then squares that number.
 */
 
+#include <iostream>
+using namespace std;
 
+int main()
+{
+  
+cout << "Please enter an integer: "<< endl;
+int num100 = 0;
+cin >> num100;
+ 
+ int numnew = 0;
+   
+		while ((num100 > 0) || (num100 < 100))
+  {
+		numnew = num100*num100;
+      cout << "Number squared is " + numnew + "."<< endl;
+	}
+  
+  if (!(num100 > 0) || !(num100 < 100))
+  {
+     cout << "Please re-enter: "<< endl;
+int num100 = 0;
+cin >> num100;
+  }
+return 0;
+}
 
