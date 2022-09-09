69ffb6cc5793379c4842ce2438676933024da630
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-08 23:49:05 -0400

    Update funcs.h

--- funcs.h
+++ funcs.h
@@ -1,9 +1,1 @@
-/*
-Author: your name
-Course: CSCI-136
-Instructor: their name
-Assignment: title, e.g., Lab1A
 
-Here, briefly, at least in one or a few sentences
-describe what the program does.
-*/
