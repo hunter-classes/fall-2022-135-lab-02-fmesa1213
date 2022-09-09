1607c2a51c2310e2933335c27d8ca452c7c4ff2e
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-08 22:20:01 -0400

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,9 +1,27 @@
 /*
-Author: your name
-Course: CSCI-136
-Instructor: their name
-Assignment: title, e.g., Lab1A
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Micheal Zamansky
+Assignment: Lab 2D
 
 Here, briefly, at least in one or a few sentences
 describe what the program does.
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int count;
+  
+
+  for (int i = 0; i < 10 ; i++)
+    {
+     cout << values[i] << " ";
+      }
+ 
+  
+    
+  return 0;
+}
