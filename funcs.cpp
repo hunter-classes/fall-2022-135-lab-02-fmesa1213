7fa5a78086f5f0dc0784b00ed2687a4ce7669631
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-08 23:40:00 -0400

    Update funcs.cpp

--- funcs.cpp
+++ funcs.cpp
@@ -1,9 +1,21 @@
 /*
-Author: your name
-Course: CSCI-136
-Instructor: their name
-Assignment: title, e.g., Lab1A
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Micheal Zamansky
+Assignment: Lab 2B
 
-Here, briefly, at least in one or a few sentences
-describe what the program does.
+I wrote a void function that will accept two integer parameters and will print the output as specied in the main lab.
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int L;
+  int U;
+  
+  void print_interval (int L, int U);
+  
+return 0;
+}
