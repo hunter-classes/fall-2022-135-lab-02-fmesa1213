9d63f4166cc9da118e3a4ce80a89926e0163f351
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-08 22:01:20 -0400

    Update edit-array.cpp

--- edit-array.cpp
+++ edit-array.cpp
@@ -1,9 +1,45 @@
 /*
-Author: your name
-Course: CSCI-136
-Instructor: their name
-Assignment: title, e.g., Lab1A
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Micheal Zamansky
+Assignment: Lab 2C
 
 Here, briefly, at least in one or a few sentences
 describe what the program does.
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+double values[10] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }; 
+
+  for (int i = 0; i < 10 ; i++)
+    {
+     cout << values[i] << " ";
+      }
+ 
+  for (int i = 0; i < 10 ; i++){
+  do{
+    
+    cout << "\n Please input index: "<< endl;
+    cin >> values[i];
+  
+    cout << "Please input new value: "<< endl;
+    cin >> values[i-1];
+      
+
+    for(int i = 0; i < 10 ; i++)
+      {
+        cout<< values[i]<< " ";
+      }
+    
+  }while (values[10] < 10);
+    }
+ 
+    
+    cout<<"Index out of range. Exit."<< endl;
+    
+  return 0;
+}
